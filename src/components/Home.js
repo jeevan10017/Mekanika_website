2d866676dc4be320e8531cae3bb0009f13d08d18
Jeevan kumar <jeevankumarkorra2005@gmail.com>
2024-02-05 15:54:42 +0530

    contrast

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,70 +1,59 @@
 import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import TeamImage from './Images/MEO3.jpg';
-import image2 from './Images/MechDTalks.jpg';
 import image1 from './Images/ME02.jpg';
+import image2 from './Images/MechDTalks.jpg';
 import image3 from './Images/ME1.jpg';
 import image4 from './Images/ME003.jpg';
 import image5 from './Images/ME5.jpg';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import './Home.css';
 
 const Home = () => {
-  const images = [
-    image1,
-    image2,
-    image3,
-    image4,
-    image5,
-  ];
+  const images = [image1, image2, image3, image4, image5];
 
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const [isHovered, setIsHovered] = useState(false);
 
-    const showNextImage = () => {
+  const showNextImage = () => {
     setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
   };
 
   const showPrevImage = () => {
     setCurrentImageIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
   };
+
   useEffect(() => {
     const interval = setInterval(() => {
-      if (!isHovered) {
-        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-      }
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
     }, 3000);
 
     return () => clearInterval(interval);
-  }, [isHovered]);
+  }, []);
 
   return (
     <section className="home-section">
-       <div
-        className="image-container"
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-      >
-      {isHovered && (
-          <React.Fragment>
-            <FontAwesomeIcon
-              icon={faChevronLeft}
-              className="arrow left"
-              onClick={showPrevImage}
-            />
-            <FontAwesomeIcon
-              icon={faChevronRight}
-              className="arrow right"
-              onClick={showNextImage}
-            />
-          </React.Fragment>
-        )}
-        <img src={images[currentImageIndex]} alt={`Image ${currentImageIndex + 1}`} />
+      <div className="image-container">
+        <FontAwesomeIcon
+          icon={faChevronLeft}
+          className="arrow left"
+          onClick={showPrevImage}
+        />
+        <div className="image-slider" style={{ transform: `translateX(-${currentImageIndex * 100}%)` }}>
+          {images.map((img, index) => (
+            <img key={index} src={img} alt={`Image ${index + 1}`} />
+          ))}
+        </div>
+        <FontAwesomeIcon
+          icon={faChevronRight}
+          className="arrow right"
+          onClick={showNextImage}
+        />
       </div>
-      
-      <section id="about" className="about-us-section"/* style={{ backgroundImage: `url(${backgroundImage})` }}*/>
+
+      <section className="about-us-section">
         <div className="about-us-overlay"></div>
         <div className="container">
+
           <div className="section-heading">
             <h2 className="mekanika-title">
               <span>M</span>
