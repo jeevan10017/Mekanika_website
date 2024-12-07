28c78e4b75f33a64701b1537a0682c44f0a45054
Jeevan Kumar Korra <132948936+jeevan10017@users.noreply.github.com>
2024-12-07 18:53:28 +0530

    Revert "required changes (#6)"
    This reverts commit ddabc39656357824cec439ba3f1fb1af6ed9865d.

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,58 +1,59 @@
-import React, { useState, useEffect, useRef } from "react";
-import "./AboutUs.css";
-import TeamImage from "./Images/mekanika_Team.jpg";
-import AOS from "aos";
-import "aos/dist/aos.css";
+import React, { useEffect } from 'react';
+import './AboutUs.css';
+import TeamImage from './Images/mekanika_Team.jpg';
+
+
 
 const AboutUs = () => {
-  // useEffect(() => {
-  //   const titleElement = document.querySelector('.mekanika-title');
-
-  //   const toggleAnimation = () => {
-  //     titleElement.classList.toggle('appear');
-  //   };
-
-  //   toggleAnimation();
-
-  //   const animationInterval = setInterval(toggleAnimation, 3000);
-
-  //   return () => clearInterval(animationInterval);
-  // }, []);
   useEffect(() => {
-    AOS.init({
-      duration: 1000, 
-      easing: 'ease-in-out',
-    });
+    const titleElement = document.querySelector('.mekanika-title');
+
+    const toggleAnimation = () => {
+      titleElement.classList.toggle('appear');
+    };
+
+    toggleAnimation();
+
+    const animationInterval = setInterval(toggleAnimation, 3000); 
+
+    return () => clearInterval(animationInterval);
   }, []);
 
   return (
-    <section className="about-us-section" id="about">
+    <section id="about" className="about-us-section">
       <div className="container">
         <div className="section-heading">
-          <div className="abouthead">About Us</div>
+          <h2 className="mekanika-title">
+            <span>M</span>
+            <span>E</span>
+            <span>K</span>
+            <span>A</span>
+            <span>N</span>
+            <span>I</span>
+            <span>K</span>
+            <span>A</span>
+          </h2>
         </div>
         <div className="content">
           <div className="text">
-            <p data-aos="fade-right">
-              Mekanika fosters an inspiring community that bridges the gap
-              between students and alumni from the Mechanical Engineering
-              Department at IIT Kharagpur. We are committed to pushing the
-              boundaries of mechanical innovation, nurturing ideas, and
-              supporting impactful projects.
+            <p>
+              Mekanika is a passionate community of students and Alummni of Department of Mechanical Engineering at IIT Kharagpur who are dedicated to advancing the field of
+              Mechanical engineering. Our mission is to building connections,
+              collaboration, and education in the world of Mechanical engineering.
             </p>
-            <p data-aos="fade-right">
-            Our mission is not only to
-              strengthen connections but to cultivate a spirit of collaboration
-              that drives forward the future of mechanical engineering.
+            <p>
+              We believe in pushing boundaries, exploring new frontiers, and
+              finding solutions to Mechanical Engineering students by conducting Events. Our team is
+              committed to excellence and continuous learning.
             </p>
           </div>
           <div className="image">
-            <img src={TeamImage} alt="Our Team" data-aos="flip-down"/>
+            <img src={TeamImage} alt="Our Team" />
           </div>
         </div>
       </div>
     </section>
   );
-};
+}
 
 export default AboutUs;
