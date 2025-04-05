eb5be43ae810adb9afba4d8cd86ea76c55570282
Jeevan kumar <jeevankumarkorra2005@gmail.com>
2025-04-06 00:48:58 +0530

    .com to .org

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,10 +1,13 @@
 const { SitemapStream, streamToPromise } = require("sitemap");
 const fs = require("fs");
 
-const sitemap = new SitemapStream({ hostname: "https://mekanikaiitkgp.com" });
+const sitemap = new SitemapStream({ hostname: "https://mekanikaiitkgp.org" });
 
 sitemap.write({ url: "/", changefreq: "daily", priority: 1.0 });
 sitemap.write({ url: "/research-areas", changefreq: "weekly", priority: 0.8 }); 
+sitemap.write({ url: "/gallery", changefreq: "monthly", priority: 0.8 }); 
+sitemap.write({ url: "/team", changefreq: "yearly", priority: 0.7 }); 
+sitemap.write({ url: "/course-material", changefreq: "monthly", priority: 0.8 }); 
 sitemap.write({ url: "/intern-chronicles", changefreq: "weekly", priority: 0.8 }); 
 sitemap.write({ url: "/core-expedition", changefreq: "weekly", priority: 0.8 }); 
 sitemap.write({ url: "/dep-map", changefreq: "monthly", priority: 0.9 }); 
